250235d2d129a51b350f8cb5d33a905e466c93b6
rodrigo <rodrigo@rodrigos-MacBook-Pro.local>
2021-05-04 17:19:46 -0300

    adding some styles and adding typePolicies to avoid warning when an item is deleted

--- graphql/front/src/components/ItemCard/index.tsx
+++ graphql/front/src/components/ItemCard/index.tsx
@@ -1,28 +1,33 @@
-import cn from 'classnames';
-import './styles.scss';
-import Box from 'src/components/UI/Box';
-import Button from 'src/components/UI/Button';
+import cn from "classnames";
+import "./styles.scss";
+import Box from "src/components/UI/Box";
+import Button from "src/components/UI/Button";
+import ScreenSize from "src/hocs/ScreenSize";
 
 interface ItemCardProps {
   className?: string;
+  isMobile?: boolean;
   title: string;
   description: string;
-  onClickDetail: ()=>void;
-  onClickDelete: ()=>void;
+  onClickDetail: () => void;
+  onClickDelete: () => void;
 }
 
 const ItemCard: React.FC<ItemCardProps> = ({
-  className = '',
+  className = "",
+  isMobile = true,
   title,
   description,
   onClickDetail,
   onClickDelete,
 }) => (
-  <div className={cn('item-card', className)}>
+  <div className={cn("item-card", { "item-card--sm": isMobile }, className)}>
     <Box className="item-card__container">
       <div className="item-card__container__info">
         <span className="item-card__container__info__dsc">{title}</span>
-        <span className="item-card__container__info__subdsc">{description}</span>
+        <span className="item-card__container__info__subdsc">
+          {description}
+        </span>
       </div>
       <Button handleClick={onClickDetail} text="Detail" />
       <Button handleClick={onClickDelete} text="Delete" />
@@ -30,4 +35,4 @@ const ItemCard: React.FC<ItemCardProps> = ({
   </div>
 );
 
-export default ItemCard;
+export default ScreenSize(ItemCard);
--- graphql/front/src/graphql/client.ts
+++ graphql/front/src/graphql/client.ts
@@ -4,7 +4,19 @@ import {
   NormalizedCacheObject,
 } from "@apollo/client";
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        items: {
+          merge(existing = [], incoming = []) {
+            return [ ...incoming ];
+          }
+        }
+      }
+    }
+  }
+});
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
